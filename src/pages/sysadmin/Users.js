78003bffd93a6fac1fbdf6238c813bd34179e96c
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-11-15 23:18:52 +0100

    added validation on registration form

--- src/pages/sysadmin/Users.js
+++ src/pages/sysadmin/Users.js
@@ -1,31 +1,32 @@
 import TemporaryNavigation from "components/SysAdmin/TemporaryNavigation";
 import UserList from "components/UserList";
-import Button from '@mui/material/Button'
+import Button from "@mui/material/Button";
 import useFetchData from "customHooks/fetchData";
-import Typography from '@mui/material/Typography'
+import Typography from "@mui/material/Typography";
 
 const Users = () => {
+  const { data, loading } = useFetchData(`Patient/all`);
 
-    const { data, loading } = useFetchData(`User/all`)
+  const handleTest = () => {
+    console.log(data);
+  };
 
-    const handleTest = () => {
-        console.log(data)
-    }
+  return (
+    <>
+      <Button variant="outlined" color="primary" onClick={handleTest}>
+        Test
+      </Button>
+      <TemporaryNavigation />
 
-    return (
-        <>
-            <Button variant="outlined" color="primary" onClick={handleTest}>
-                Test
-            </Button>
-            <TemporaryNavigation />
-
-            {loading ?
-                <Typography variant="h6" color="initial">Loading..</Typography>
-                :
-                <UserList users={data} />
-            }
-        </>
-    );
-}
+      {loading ? (
+        <Typography variant="h6" color="initial">
+          Loading..
+        </Typography>
+      ) : (
+        <UserList users={data} />
+      )}
+    </>
+  );
+};
 
 export default Users;
