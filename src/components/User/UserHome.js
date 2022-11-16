3c4353470eee26b738ee09d61db44571331137da
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-11-16 18:34:09 +0100

    added feature for display all blood banks with sorting, without design

---
+++ src/components/User/UserHome.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import "./UserHome.css";
+
+const UserHome = (props) => {
+  const [data, setData] = useState(props.bloodBanks);
+  const [order, setOrder] = useState("ASC");
+
+  const sorting = (col) => {
+    if (order === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("DSC");
+    }
+    if (order === "DSC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("ASC");
+    }
+  };
+
+  const sortingSpecial = (col, col1) => {
+    if (order === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col][col1].toLowerCase() > b[col][col1].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("DSC");
+    }
+    if (order === "DSC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col][col1].toLowerCase() < b[col][col1].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("ASC");
+    }
+  };
+  return (
+    <div>
+      <h1>Blood banks</h1>
+      <div className="container">
+        <table border={1}>
+          <thead>
+            <tr>
+              <th onClick={() => sorting("name")}>Name</th>
+              <th onClick={() => sortingSpecial("address", "street")}>
+                Street
+              </th>
+              <th onClick={() => sortingSpecial("address", "city")}>City</th>
+              <th onClick={() => sortingSpecial("address", "country")}>
+                Country
+              </th>
+              <th>Rating</th>
+              <th onClick={() => sorting("description")}>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((bloodBank) => (
+              <tr key={bloodBank.id}>
+                <td>{bloodBank.name}</td>
+                <td>
+                  {bloodBank.address.street + " " + bloodBank.address.number}
+                </td>
+                <td>{bloodBank.address.city}</td>
+                <td>{bloodBank.address.country}</td>
+                <td>{bloodBank.rating}</td>
+                <td>{bloodBank.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserHome;
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,9 +1,9 @@
+import UserHome from "components/User/UserHome";
+import useFetchData from "customHooks/fetchData";
+import Loading from "components/Loading";
 const User = () => {
-    return ( 
-        <>
-        Welcome user!
-        </>
-     );
-}
- 
+  const { data, loading } = useFetchData(`BloodBank/all`);
+  return <>{loading ? <Loading /> : <UserHome bloodBanks={data} />}</>;
+};
+
 export default User;
