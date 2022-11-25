603d1ec12f882759419f361f0cb1a2b1ff09bbed
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-11-25 10:28:08 +0100

    added sort and pagination(backend) on blood banks table

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,16 +3,13 @@ import useFetchData from "customHooks/fetchData";
 import Loading from "components/Loading";
 import { Link } from "react-router-dom";
 const User = () => {
-  const { data, loading } = useFetchData(`BloodBank/all`);
-  return <>
-    <Link to={"/update-patient"}>
-      UpdatePatient
-    </Link>
-    <Link to={"/poll"}>
-      Poll
-    </Link>
-    {loading ? <Loading /> : <UserHome bloodBanks={data} />}
-  </>;
+  return (
+    <>
+      <Link to={"/update-patient"}>UpdatePatient</Link>
+      <Link to={"/poll"}>Poll</Link>
+      <UserHome />
+    </>
+  );
 };
 
 export default User;
