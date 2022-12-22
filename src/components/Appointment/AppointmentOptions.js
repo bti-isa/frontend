f549cdc86f7feb3a2549277a563e9c5da699871b
Andrija25 <andrija.d.stanisic@gamil.com>
2022-12-22 23:11:37 +0100

    added punish patient feature

--- src/components/Appointment/AppointmentOptions.js
+++ src/components/Appointment/AppointmentOptions.js
@@ -1,12 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import classes from './AppointmentOptions.module.css';
+import CONSTANTS from 'constants/constants';
+import {axiosInstance} from '../../config/https'
+import { useNavigate } from 'react-router';
 
 const AppointmetOptions = (props) =>{
+    const [data, setData] = useState(props.appointment)
+    const navigate = useNavigate();
+
+    const handlePunish = () =>{
+        const patientId = data.patient.id;
+        axiosInstance
+            .put(`${CONSTANTS.API}Patient/punish/${patientId}`).then((res) => {
+                navigate('/user')
+            });
+    }
+
+    const handleCancel = () =>{
+    }
+
+    const handleStart = () =>{
+    }
+
     return(
         <div className={classes["option-container"]}>
-            <button>Punish</button>
-            <button>Cancel</button>
-            <button>Start</button>
+            <button onClick={handlePunish}>Punish</button>
+            <button onClick={handleCancel}>Cancel</button>
+            <button onClick={handleStart}>Start</button>
         </div>
     );
 }
--- src/pages/appointment/AppointmentDetail.js
+++ src/pages/appointment/AppointmentDetail.js
@@ -6,14 +6,17 @@ import useFetchData from 'customHooks/fetchData';
 import { useEffect, useState } from 'react';
 import classes from './AppointmentDetail.module.css'
 import Loading from "components/Loading";
+import { useParams } from 'react-router';
 
 const AppointmentDetail = () =>{
+    const params = useParams();
+    //${params.id}
     const {data,loading} = useFetchData(`Appointment/1`)
     return(
         <>
             {loading  ? <Loading /> :  <div className={classes["main-container"]}>
                 <div className={classes["left"]}><AppointmentDetailComponent appointment={data} /></div>
-                <div className={classes["right"]}><AppointmetOptions /></div>
+                <div className={classes["right"]}><AppointmetOptions appointment={data} /></div>
                 </div>
             }
         </>
