d92c199f4f9fec35cb7c992bed777bb248018152
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-01-25 02:03:29 +0100

    added event click on calendar

--- src/components/Appointment/AppointmentRealizationComponent.js
+++ src/components/Appointment/AppointmentRealizationComponent.js
@@ -1,55 +1,57 @@
 import { useRef, useState } from "react";
-import classes from './AppointmentRealizationComponent.module.css'
-import { axiosInstance } from '../../config/https'
+import classes from "./AppointmentRealizationComponent.module.css";
+import { axiosInstance } from "../../config/https";
 import CONSTANTS from "constants/constants";
 import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
 
-const AppointmentRealizationComponent = (props) =>{
-    const [data, setData] = useState(props.appointment);
-    const inputDesc = useRef();
-    const inputEquipment = useRef();
-    const inputBlood = useRef();
-    const navigate = useNavigate();
+const AppointmentRealizationComponent = (props) => {
+  const [data, setData] = useState(props.appointment);
+  const inputDesc = useRef();
+  const inputEquipment = useRef();
+  const inputBlood = useRef();
+  const navigate = useNavigate();
 
-    const handleFinish = () =>{
-        const dto = {
-            id: data.id,
-            patientId: data.patient.id,
-            bloodBankId: data.bloodBank.id,
-            reportDescription: inputDesc.current.value,
-            equipment: inputEquipment.current.value,
-            bloodQuantity: inputBlood.current.value
-        }
+  const handleFinish = () => {
+    const dto = {
+      id: data.id,
+      reportDescription: inputDesc.current.value,
+      equipment: inputEquipment.current.value,
+      bloodQuantity: inputBlood.current.value,
+    };
 
-        axiosInstance
-        .patch(`${CONSTANTS.API}Appointment/finish`, dto).then((res) => {
-            alert("Appointment has been finished!")
-            navigate('/user')
-        });
-    }
+    axiosInstance
+      .patch(`${CONSTANTS.API}Appointment/finish`, dto)
+      .then((res) => {
+        toast("Appointment has been finished!");
+        navigate("/user");
+      });
+  };
 
-     return(
-        <div className={classes["deail-container-re"]}>
-            <div className={classes.titlere}>APPOINTMENT REALIZATION</div>
-            <div className={classes.infore}>
-                <div className={classes["info-item-re"]}>
-                    <label>Used Equipment:</label>
-                    <input type="number" min="0" ref={inputEquipment}/>
-                </div>
-                 <div className={classes["info-item-re"]}>
-                    <label>Blood Quantity:</label>
-                    <input type="number" min="0" ref={inputBlood} />
-                </div>
-                <div className={classes["info-item-re"]}>
-                    <label>Report:</label>
-                </div>
-                    <textarea ref={inputDesc} name="Text1" cols="60" rows="5"></textarea>
-            </div>
-            <div className={classes["finish-container"]}>
-                <button className={classes['finish-button']} onClick={handleFinish}>Finish</button>
-            </div>
+  return (
+    <div className={classes["deail-container-re"]}>
+      <div className={classes.titlere}>APPOINTMENT REALIZATION</div>
+      <div className={classes.infore}>
+        <div className={classes["info-item-re"]}>
+          <label>Used Equipment:</label>
+          <input type="number" min="0" ref={inputEquipment} />
         </div>
-    );
-}
+        <div className={classes["info-item-re"]}>
+          <label>Blood Quantity:</label>
+          <input type="number" min="0" ref={inputBlood} />
+        </div>
+        <div className={classes["info-item-re"]}>
+          <label>Report:</label>
+        </div>
+        <textarea ref={inputDesc} name="Text1" cols="60" rows="5"></textarea>
+      </div>
+      <div className={classes["finish-container"]}>
+        <button className={classes["finish-button"]} onClick={handleFinish}>
+          Finish
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default AppointmentRealizationComponent;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-    API: `http://localhost:8080/api/`
-}
+  API: `http://localhost:8080/api/`,
+};
 
-export default CONSTANTS
+export default CONSTANTS;
