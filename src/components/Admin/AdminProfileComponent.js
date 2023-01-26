aabb8bd2c25bbcdca99d53387672109daacdbe14
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-01-26 18:17:51 +0100

    added styling

--- src/components/Admin/AdminProfileComponent.js
+++ src/components/Admin/AdminProfileComponent.js
@@ -1,14 +1,102 @@
+import { axiosInstance } from "config/https";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import classes from "./AdminProfileComponent.css";
 
 const AdminProfileComponent = (props) => {
   const [data, setData] = useState(props.data);
+  const [disabled, setDisabled] = useState(true);
+  const [showEditButton, setShowEditButton] = useState(true);
+  console.log(data);
+  const [userInput, setUserInput] = useState({
+    id: props.data.id,
+    enteredFirstName: props.data.firstname,
+    enteredLastName: props.data.lastname,
+    enteredPhone: props.data.phoneNumber,
+    enteredCity: props.data.address.city,
+    enteredStreet: props.data.address.street,
+    enteredNumber: props.data.address.number,
+    enteredPostalCode: props.data.address.postalCode,
+  });
   const navigate = useNavigate();
 
   const handleUpdate = () => {
-    navigate(`/update-admin/${data.id}`);
+    setShowEditButton(false);
+    setDisabled(false);
   };
+
+  const handleCancel = () => {
+    setShowEditButton(true);
+    setDisabled(true);
+  };
+
+  const firstNameChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredFirstName: event.target.value,
+    });
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredLastName: event.target.value,
+    });
+  };
+
+  const phoneChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredPhone: event.target.value,
+    });
+  };
+
+  const cityChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredCity: event.target.value,
+    });
+  };
+
+  const streetChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredStreet: event.target.value,
+    });
+  };
+
+  const numberChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredNumber: event.target.value,
+    });
+  };
+
+  const postalCodeChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredPostalCode: Number(event.target.value),
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axiosInstance
+      .put(`http://localhost:8080/api/Admin/profile-update`, userInput)
+      .then((response) => {
+        setDisabled(true);
+        setShowEditButton(true);
+        if (response) {
+          toast("You have successfully updated your account info");
+          navigate("/admin-profile");
+          return;
+        }
+
+        toast("Something went wrong, please try again");
+      });
+  };
+
   return (
     <div className="admin-container">
       <div className="admin-title">My Account</div>
@@ -17,18 +105,20 @@ const AdminProfileComponent = (props) => {
           <div className="data-item">
             <label className="data-label">First Name:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.firstname}
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredFirstName}
+              onChange={firstNameChangeHandler}
               type="text"
             />
           </div>
           <div className="data-item">
             <label className="data-label">Last Name:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.lastname}
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredLastName}
+              onChange={lastNameChangeHandler}
               type="text"
             />
           </div>
@@ -64,54 +154,79 @@ const AdminProfileComponent = (props) => {
           <div className="data-item">
             <label className="data-label">Phone Number:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.phoneNumber}
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredPhone}
+              onChange={phoneChangeHandler}
               type="text"
             />
           </div>
           <div className="data-item">
             <label className="data-label">City:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.address.city}
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredCity}
+              onChange={cityChangeHandler}
               type="text"
             />
           </div>
           <div className="data-item">
-            <label className="data-label">Address:</label>
+            <label className="data-label">Street:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.address.street}
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredStreet}
+              onChange={streetChangeHandler}
               type="text"
             />
           </div>
           <div className="data-item">
             <label className="data-label">Number:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.address.number}
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredNumber}
+              onChange={numberChangeHandler}
               type="text"
             />
           </div>
           <div className="data-item">
             <label className="data-label">Postal Code:</label>
             <input
-              className="data-input"
-              disabled
-              value={data.address.postalCode}
-              type="text"
+              className={disabled ? "data-input" : "data-input editable"}
+              disabled={disabled}
+              value={userInput.enteredPostalCode}
+              onChange={postalCodeChangeHandler}
+              type="number"
             />
           </div>
         </div>
       </div>
       <div className="admin-update-container">
-        <button onClick={handleUpdate} className="admin-update-button">
-          Update
-        </button>
+        {showEditButton && (
+          <>
+            <button
+              onClick={handleUpdate}
+              className="admin-update-button password-change"
+            >
+              Change Password
+            </button>
+            <button onClick={handleUpdate} className="admin-update-button">
+              Edit
+            </button>
+          </>
+        )}
+        {!showEditButton && (
+          <>
+            <button className="admin-update-button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button className="admin-update-button" onClick={handleSubmit}>
+              Submit
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
